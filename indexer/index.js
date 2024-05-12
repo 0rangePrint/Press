8a7519f648e579973edfc0000c5359f665e9d720
0rangePrint <jaimenajera2007@gmail.com>
2024-05-12 14:33:17 -0400

    Add files via upload

--- indexer/index.js
+++ indexer/index.js
@@ -1,6 +1,32 @@
 function entername() {
-let entName = document.getElementById('entName').value;
-if (entName=='' ){
-    alert('Youre cute as hellll!!!!!!!!!!!');
+var entName = document.getElementById('entName').value;
+if (entName=='Jake' ){
+    alert('7-10');
+}
+else if (entName==''){
+    alert('Please enter a name!');
+}
+else if(entName=='Jaime'){
+    alert('5/10');
+}
+else if(entName=='Miguel'){
+    alert('10/10')
+}
+else if(entName=='Jorge'){
+    alert('.5/10')
+}
+else if(entName=='Vale'){
+    alert('10/10')
+}
+else if(entName=='Lulu'){
+    alert('10/10')
+}
+else if(entName=='Ed Sheeran'){
+    alert('Infinity')
+}
+else if(entName=='Alina'){
+    alert('10/10')
+} else {
+    alert('Unknown, Names must start with Uppercases!');
 }
 }
